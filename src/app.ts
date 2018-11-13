0de9b664eb2b19713ca4f170598168c210adfaac
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-11-13 09:00:43 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-const express = require("express");
-const compression = require("compression");
+import * as express from "express";
+import * as compression from "compression";
 
 const app = express();
 
-const hello = require("./hello");
+const index = require("./routes/index");
+const hello = require("./routes/hello");
 
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", hello);
+app.use("/", index);
+app.use("/hello", hello);
 
 export default app;
