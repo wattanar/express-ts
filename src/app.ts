29a026fe5234dc9502c9d65e6f135443caf066f4
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-11-16 11:37:07 +0700

    add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import * as express from "express";
 import * as compression from "compression";
+import * as helmet from "helmet";
+// routes
 import index from "./routes/index";
 import hello from "./routes/hello";
 
 const app = express();
 
 app.use(compression());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
