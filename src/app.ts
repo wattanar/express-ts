6d5c55a29eff2bb0b5c9b8f9cff3911687ca4767
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-11-16 14:02:14 +0700

    config static

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as compression from "compression";
 import * as helmet from "helmet";
+import * as path from "path";
 // routes
 import index from "./routes/index";
 import hello from "./routes/hello";
@@ -11,6 +12,7 @@ app.use(compression());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "static")));
 
 app.use("/", index);
 app.use("/hello", hello);
