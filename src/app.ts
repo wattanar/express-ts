6d7f0181099a531436c92780eea4947478720549
Wattana Ruengmucha <iamwattanas@gmail.com>
2019-05-13 09:19:23 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "static")));
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 app.use("/", index);
