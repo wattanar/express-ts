ef29951b5d0e500a72a821096a80c47295a2fc63
Wattana Ruengmucha <iamwattanas@gmail.com>
2019-05-13 13:58:34 +0700

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 const router = express.Router();
 
 router.get("/", (req: express.Request, res: express.Response) => {
-  res.render("page", { page: "index", title: "Hello, World!" });
+  res.render("layouts/main", { page: "index", title: "Hello, World!" });
 });
 
 export default router;
